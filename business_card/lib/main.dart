36c6774bcf425b848a5b06a60936fb57d850bbb0
henry <developer.genuis@gmail.com>
2024-05-20 01:49:35 +0300

    first

---
+++ business_card/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(businesscard());
+}
+
+class businesscard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF2B475E),
+      ),
+    );
+  }
+}
